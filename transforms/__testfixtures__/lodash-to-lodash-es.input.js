0a79382e4f13889d7a7c8719be593ffe29d70b44
sasaplus1 <sasaplus1@gmail.com>
2019-06-05 18:47:33 +0900

    update test

--- transforms/__testfixtures__/lodash-to-lodash-es.input.js
+++ transforms/__testfixtures__/lodash-to-lodash-es.input.js
@@ -1,2 +1,6 @@
+/**
+ * @file comment
+ */
 import get from 'lodash/get';
+// NOTE: comment
 import _ from 'lodash';
--- transforms/__testfixtures__/lodash-to-lodash-es.output.js
+++ transforms/__testfixtures__/lodash-to-lodash-es.output.js
@@ -1,2 +1,6 @@
+/**
+ * @file comment
+ */
 import get from 'lodash-es/get';
+// NOTE: comment
 import _ from 'lodash-es';
