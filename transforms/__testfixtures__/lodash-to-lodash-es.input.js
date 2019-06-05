1b00285d77ff9028925a396500b7e6950e9eaf0f
sasaplus1 <sasaplus1@gmail.com>
2019-06-05 18:49:54 +0900

    improve test

--- transforms/__testfixtures__/lodash-to-lodash-es.input.js
+++ transforms/__testfixtures__/lodash-to-lodash-es.input.js
@@ -4,3 +4,7 @@
 import get from 'lodash/get';
 // NOTE: comment
 import _ from 'lodash';
+// FIXME: comment
+import * as lodash from 'lodash';
+// TODO: comment
+import { set } from 'lodash';
--- transforms/__testfixtures__/lodash-to-lodash-es.output.js
+++ transforms/__testfixtures__/lodash-to-lodash-es.output.js
@@ -4,3 +4,7 @@
 import get from 'lodash-es/get';
 // NOTE: comment
 import _ from 'lodash-es';
+// FIXME: comment
+import * as lodash from 'lodash-es';
+// TODO: comment
+import { set } from 'lodash-es';
